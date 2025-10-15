7ef611db8772b77c4a79f109f8e9c4a4a86f8b63
KarpenkoArtem <TyuiuKarpenkoAL@outlook.com>
2025-10-15 14:30:31 +0500

    Add project files.

---
+++ Tyuiu.KarpenkoAL.Sprint6.Task0.V17.Lib/DataService.cs
@@ -0,0 +1,14 @@
+﻿using System.Text;
+using tyuiu.cources.programming.interfaces.Sprint6;
+
+namespace Tyuiu.KarpenkoAL.Sprint6.Task0.V17.Lib
+{
+    public class DataService : ISprint6Task0V17
+    {
+        public double Calculate(int x)
+        {
+            double res = Math.Round((Math.Exp(x)) / x, 3);
+            return res;
+        }
+    }
+}
---
+++ Tyuiu.KarpenkoAL.Sprint6.Task0.V17.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.KarpenkoAL.Sprint6.Task0.V17.Lib;
+
+
+namespace Tyuiu.KarpenkoAL.Sprint6.Task0.V17.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCalc()
+        {
+            DataService ds = new DataService();
+            double res = ds.Calculate(3);
+            double wait = 6.695;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
