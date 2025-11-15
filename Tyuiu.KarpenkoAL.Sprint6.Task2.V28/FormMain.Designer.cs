0fc05d6be7dd11aefde9fe0fbf6bb4f7e327a439
KarpenkoArtem <TyuiuKarpenkoAL@outlook.com>
2025-11-15 13:13:17 +0500

    Task2

--- Tyuiu.KarpenkoAL.Sprint6.Task2.V28/FormMain.Designer.cs
+++ Tyuiu.KarpenkoAL.Sprint6.Task2.V28/FormMain.Designer.cs
@@ -42,11 +42,11 @@ namespace Tyuiu.KarpenkoAL.Sprint6.Task2.V28
             buttonHelp = new Button();
             buttonDone = new Button();
             dataGridViewFunctionTab = new DataGridView();
-            DataGridViewFunctionTab_X = new DataGridViewTextBoxColumn();
-            DataGridViewFunctionTab_Fx = new DataGridViewTextBoxColumn();
             groupBoxDataOutput = new GroupBox();
             groupBoxResult = new GroupBox();
             chartResult = new FormsPlot();
+            DataGridViewFunctionTab_X = new DataGridViewTextBoxColumn();
+            DataGridViewFunctionTab_Fx = new DataGridViewTextBoxColumn();
             groupBoxWithRequirements.SuspendLayout();
             groupBoxDataInput.SuspendLayout();
             groupBoxStopStep.SuspendLayout();
@@ -59,20 +59,23 @@ namespace Tyuiu.KarpenkoAL.Sprint6.Task2.V28
             // groupBoxWithRequirements
             // 
             groupBoxWithRequirements.Controls.Add(textBoxTask);
-            groupBoxWithRequirements.Location = new Point(12, 12);
+            groupBoxWithRequirements.Location = new Point(14, 16);
+            groupBoxWithRequirements.Margin = new Padding(3, 4, 3, 4);
             groupBoxWithRequirements.Name = "groupBoxWithRequirements";
-            groupBoxWithRequirements.Size = new Size(458, 129);
+            groupBoxWithRequirements.Padding = new Padding(3, 4, 3, 4);
+            groupBoxWithRequirements.Size = new Size(523, 172);
             groupBoxWithRequirements.TabIndex = 0;
             groupBoxWithRequirements.TabStop = false;
             groupBoxWithRequirements.Text = "Условие";
             // 
             // textBoxTask
             // 
-            textBoxTask.Location = new Point(6, 22);
+            textBoxTask.Location = new Point(7, 29);
+            textBoxTask.Margin = new Padding(3, 4, 3, 4);
             textBoxTask.Multiline = true;
             textBoxTask.Name = "textBoxTask";
             textBoxTask.ReadOnly = true;
-            textBoxTask.Size = new Size(446, 101);
+            textBoxTask.Size = new Size(509, 133);
             textBoxTask.TabIndex = 0;
             textBoxTask.Text = "Протабулировать фукнцию ((sin(x)-(2*x))/((3*x)-1))+sin(x)-(3*x)+2 на заданном диапазоне.\r\nРезультат вывести в DataGridView и построить график функции";
             // 
@@ -80,9 +83,11 @@ namespace Tyuiu.KarpenkoAL.Sprint6.Task2.V28
             // 
             groupBoxDataInput.Controls.Add(groupBoxStopStep);
             groupBoxDataInput.Controls.Add(groupBoxStartStep);
-            groupBoxDataInput.Location = new Point(18, 209);
+            groupBoxDataInput.Location = new Point(21, 279);
+            groupBoxDataInput.Margin = new Padding(3, 4, 3, 4);
             groupBoxDataInput.Name = "groupBoxDataInput";
-            groupBoxDataInput.Size = new Size(224, 93);
+            groupBoxDataInput.Padding = new Padding(3, 4, 3, 4);
+            groupBoxDataInput.Size = new Size(256, 124);
             groupBoxDataInput.TabIndex = 1;
             groupBoxDataInput.TabStop = false;
             groupBoxDataInput.Text = "Ввод данных:";
@@ -90,45 +95,52 @@ namespace Tyuiu.KarpenkoAL.Sprint6.Task2.V28
             // groupBoxStopStep
             // 
             groupBoxStopStep.Controls.Add(textBoxStopStep);
-            groupBoxStopStep.Location = new Point(115, 22);
+            groupBoxStopStep.Location = new Point(131, 29);
+            groupBoxStopStep.Margin = new Padding(3, 4, 3, 4);
             groupBoxStopStep.Name = "groupBoxStopStep";
-            groupBoxStopStep.Size = new Size(103, 65);
+            groupBoxStopStep.Padding = new Padding(3, 4, 3, 4);
+            groupBoxStopStep.Size = new Size(118, 87);
             groupBoxStopStep.TabIndex = 1;
             groupBoxStopStep.TabStop = false;
             groupBoxStopStep.Text = "Конец шага";
             // 
             // textBoxStopStep
             // 
-            textBoxStopStep.Location = new Point(0, 22);
+            textBoxStopStep.Location = new Point(0, 29);
+            textBoxStopStep.Margin = new Padding(3, 4, 3, 4);
             textBoxStopStep.Name = "textBoxStopStep";
-            textBoxStopStep.Size = new Size(103, 23);
+            textBoxStopStep.Size = new Size(117, 27);
             textBoxStopStep.TabIndex = 0;
             textBoxStopStep.KeyPress += NumberInput;
             // 
             // groupBoxStartStep
             // 
             groupBoxStartStep.Controls.Add(textBoxStartStep);
-            groupBoxStartStep.Location = new Point(6, 22);
+            groupBoxStartStep.Location = new Point(7, 29);
+            groupBoxStartStep.Margin = new Padding(3, 4, 3, 4);
             groupBoxStartStep.Name = "groupBoxStartStep";
-            groupBoxStartStep.Size = new Size(103, 65);
+            groupBoxStartStep.Padding = new Padding(3, 4, 3, 4);
+            groupBoxStartStep.Size = new Size(118, 87);
             groupBoxStartStep.TabIndex = 0;
             groupBoxStartStep.TabStop = false;
             groupBoxStartStep.Text = "Старт шага";
             // 
             // textBoxStartStep
             // 
-            textBoxStartStep.Location = new Point(0, 22);
+            textBoxStartStep.Location = new Point(0, 29);
+            textBoxStartStep.Margin = new Padding(3, 4, 3, 4);
             textBoxStartStep.Name = "textBoxStartStep";
-            textBoxStartStep.Size = new Size(103, 23);
+            textBoxStartStep.Size = new Size(117, 27);
             textBoxStartStep.TabIndex = 0;
             textBoxStartStep.KeyPress += NumberInput;
             // 
             // buttonHelp
             // 
             buttonHelp.BackColor = System.Drawing.Color.FromArgb(128, 255, 255);
-            buttonHelp.Location = new Point(294, 231);
+            buttonHelp.Location = new Point(336, 308);
+            buttonHelp.Margin = new Padding(3, 4, 3, 4);
             buttonHelp.Name = "buttonHelp";
-            buttonHelp.Size = new Size(66, 43);
+            buttonHelp.Size = new Size(75, 57);
             buttonHelp.TabIndex = 2;
             buttonHelp.Text = "Справка";
             buttonHelp.UseVisualStyleBackColor = false;
@@ -137,9 +149,10 @@ namespace Tyuiu.KarpenkoAL.Sprint6.Task2.V28
             // buttonDone
             // 
             buttonDone.BackColor = System.Drawing.Color.LawnGreen;
-            buttonDone.Location = new Point(366, 231);
+            buttonDone.Location = new Point(418, 308);
+            buttonDone.Margin = new Padding(3, 4, 3, 4);
             buttonDone.Name = "buttonDone";
-            buttonDone.Size = new Size(98, 43);
+            buttonDone.Size = new Size(112, 57);
             buttonDone.TabIndex = 3;
             buttonDone.Text = "Выполнить";
             buttonDone.UseVisualStyleBackColor = false;
@@ -147,34 +160,26 @@ namespace Tyuiu.KarpenkoAL.Sprint6.Task2.V28
             // 
             // dataGridViewFunctionTab
             // 
+            dataGridViewFunctionTab.AllowUserToResizeColumns = false;
+            dataGridViewFunctionTab.AllowUserToResizeRows = false;
             dataGridViewFunctionTab.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             dataGridViewFunctionTab.Columns.AddRange(new DataGridViewColumn[] { DataGridViewFunctionTab_X, DataGridViewFunctionTab_Fx });
-            dataGridViewFunctionTab.Location = new Point(6, 22);
+            dataGridViewFunctionTab.Location = new Point(7, 29);
+            dataGridViewFunctionTab.Margin = new Padding(3, 4, 3, 4);
             dataGridViewFunctionTab.Name = "dataGridViewFunctionTab";
             dataGridViewFunctionTab.RowHeadersVisible = false;
-            dataGridViewFunctionTab.Size = new Size(120, 246);
+            dataGridViewFunctionTab.RowHeadersWidth = 51;
+            dataGridViewFunctionTab.Size = new Size(137, 328);
             dataGridViewFunctionTab.TabIndex = 4;
-            // 
-            // DataGridViewFunctionTab_X
-            // 
-            DataGridViewFunctionTab_X.HeaderText = "X";
-            DataGridViewFunctionTab_X.Name = "DataGridViewFunctionTab_X";
-            DataGridViewFunctionTab_X.ReadOnly = true;
-            DataGridViewFunctionTab_X.Width = 50;
-            // 
-            // DataGridViewFunctionTab_Fx
-            // 
-            DataGridViewFunctionTab_Fx.HeaderText = "F(X)";
-            DataGridViewFunctionTab_Fx.Name = "DataGridViewFunctionTab_Fx";
-            DataGridViewFunctionTab_Fx.ReadOnly = true;
-            DataGridViewFunctionTab_Fx.Width = 50;
             // 
             // groupBoxDataOutput
             // 
             groupBoxDataOutput.Controls.Add(groupBoxResult);
-            groupBoxDataOutput.Location = new Point(476, 12);
+            groupBoxDataOutput.Location = new Point(544, 16);
+            groupBoxDataOutput.Margin = new Padding(3, 4, 3, 4);
             groupBoxDataOutput.Name = "groupBoxDataOutput";
-            groupBoxDataOutput.Size = new Size(541, 290);
+            groupBoxDataOutput.Padding = new Padding(3, 4, 3, 4);
+            groupBoxDataOutput.Size = new Size(618, 387);
             groupBoxDataOutput.TabIndex = 5;
             groupBoxDataOutput.TabStop = false;
             groupBoxDataOutput.Text = "Вывод данных";
@@ -183,31 +188,52 @@ namespace Tyuiu.KarpenkoAL.Sprint6.Task2.V28
             // 
             groupBoxResult.Controls.Add(chartResult);
             groupBoxResult.Controls.Add(dataGridViewFunctionTab);
-            groupBoxResult.Location = new Point(0, 22);
+            groupBoxResult.Location = new Point(0, 29);
+            groupBoxResult.Margin = new Padding(3, 4, 3, 4);
             groupBoxResult.Name = "groupBoxResult";
-            groupBoxResult.Size = new Size(541, 268);
+            groupBoxResult.Padding = new Padding(3, 4, 3, 4);
+            groupBoxResult.Size = new Size(618, 357);
             groupBoxResult.TabIndex = 0;
             groupBoxResult.TabStop = false;
             groupBoxResult.Text = "Результат:";
             // 
             // chartResult
             // 
-            chartResult.Location = new Point(132, 22);
+            chartResult.DisplayScale = 1.25F;
+            chartResult.Location = new Point(151, 29);
+            chartResult.Margin = new Padding(3, 4, 3, 4);
             chartResult.Name = "chartResult";
-            chartResult.Size = new Size(409, 246);
+            chartResult.Size = new Size(467, 328);
             chartResult.TabIndex = 5;
+            // 
+            // DataGridViewFunctionTab_X
+            // 
+            DataGridViewFunctionTab_X.HeaderText = "X";
+            DataGridViewFunctionTab_X.MinimumWidth = 6;
+            DataGridViewFunctionTab_X.Name = "DataGridViewFunctionTab_X";
+            DataGridViewFunctionTab_X.ReadOnly = true;
+            DataGridViewFunctionTab_X.Width = 50;
+            // 
+            // DataGridViewFunctionTab_Fx
+            // 
+            DataGridViewFunctionTab_Fx.HeaderText = "F(X)";
+            DataGridViewFunctionTab_Fx.MinimumWidth = 6;
+            DataGridViewFunctionTab_Fx.Name = "DataGridViewFunctionTab_Fx";
+            DataGridViewFunctionTab_Fx.ReadOnly = true;
+            DataGridViewFunctionTab_Fx.Width = 60;
             // 
             // FormMain
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1029, 314);
+            ClientSize = new Size(1176, 419);
             Controls.Add(groupBoxDataOutput);
             Controls.Add(buttonDone);
             Controls.Add(buttonHelp);
             Controls.Add(groupBoxDataInput);
             Controls.Add(groupBoxWithRequirements);
             FormBorderStyle = FormBorderStyle.FixedSingle;
+            Margin = new Padding(3, 4, 3, 4);
             MaximizeBox = false;
             Name = "FormMain";
             StartPosition = FormStartPosition.CenterScreen;
@@ -239,8 +265,8 @@ namespace Tyuiu.KarpenkoAL.Sprint6.Task2.V28
         private DataGridView dataGridViewFunctionTab;
         private GroupBox groupBoxDataOutput;
         private GroupBox groupBoxResult;
+        private FormsPlot chartResult;
         private DataGridViewTextBoxColumn DataGridViewFunctionTab_X;
         private DataGridViewTextBoxColumn DataGridViewFunctionTab_Fx;
-        private FormsPlot chartResult;
     }
 }
